28e2ecb7f6eaf66abbf2d1050297d1f031fabd16
Yury Golub <yuragolub21@gmail.com>
2022-03-14 20:10:50 +0300

    Implement the GetCurrentProducts and GetMostExpensiveProductsReport methods.

--- Northwind.ReportingServices.OData/ProductReports/ProductReportService.cs
+++ Northwind.ReportingServices.OData/ProductReports/ProductReportService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Data.Services.Client;
 using System.Linq;
 using System.Threading.Tasks;
-using NorthwindProduct = NorthwindModel.Product;
+using NorthwindModel;
+
+#pragma warning disable CA2008 // Do not create tasks without passing a TaskScheduler
 
 namespace Northwind.ReportingServices.OData.ProductReports
 {
@@ -12,7 +14,7 @@ namespace Northwind.ReportingServices.OData.ProductReports
     /// </summary>
     public class ProductReportService
     {
-        private readonly NorthwindModel.NorthwindEntities entities;
+        private readonly NorthwindEntities entities;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ProductReportService"/> class.
@@ -20,7 +22,12 @@ namespace Northwind.ReportingServices.OData.ProductReports
         /// <param name="northwindServiceUri">An URL to Northwind OData service.</param>
         public ProductReportService(Uri northwindServiceUri)
         {
-            this.entities = new NorthwindModel.NorthwindEntities(northwindServiceUri ?? throw new ArgumentNullException(nameof(northwindServiceUri)));
+            if (northwindServiceUri is null)
+            {
+                throw new ArgumentNullException(nameof(northwindServiceUri));
+            }
+
+            this.entities = new NorthwindEntities(northwindServiceUri);
         }
 
         /// <summary>
@@ -29,7 +36,43 @@ namespace Northwind.ReportingServices.OData.ProductReports
         /// <returns>Returns <see cref="ProductReport{T}"/>.</returns>
         public async Task<ProductReport<ProductPrice>> GetCurrentProducts()
         {
-            return new ProductReport<ProductPrice>(Array.Empty<ProductPrice>());
+            var query = (DataServiceQuery<ProductPrice>)(
+                from p in this.entities.Products
+                where !p.Discontinued
+                orderby p.ProductName
+                select new ProductPrice
+                {
+                    Name = p.ProductName,
+                    Price = p.UnitPrice ?? 0,
+                });
+
+            var result = await Task<IEnumerable<ProductPrice>>
+                .Factory
+                .FromAsync(query.BeginExecute(null, query), ar => query.EndExecute(ar))
+                .ContinueWith(x => ContinuePage(x.Result));
+
+            return new ProductReport<ProductPrice>(result);
+
+            IEnumerable<ProductPrice> ContinuePage(IEnumerable<ProductPrice> response)
+            {
+                foreach (var element in response)
+                {
+                    yield return element;
+                }
+
+                if ((response as QueryOperationResponse)?.GetContinuation() is DataServiceQueryContinuation<ProductPrice> continuation)
+                {
+                    var innerTask = Task<IEnumerable<ProductPrice>>
+                        .Factory
+                        .FromAsync(this.entities.BeginExecute(continuation, null, null), this.entities.EndExecute<ProductPrice>)
+                        .ContinueWith(t => ContinuePage(t.Result));
+
+                    foreach (var productPrice in innerTask.Result)
+                    {
+                        yield return productPrice;
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -39,7 +82,29 @@ namespace Northwind.ReportingServices.OData.ProductReports
         /// <returns>Returns <see cref="ProductReport{ProductPrice}"/>.</returns>
         public async Task<ProductReport<ProductPrice>> GetMostExpensiveProductsReport(int count)
         {
-            return new ProductReport<ProductPrice>(Array.Empty<ProductPrice>());
+            var query = (DataServiceQuery<ProductPrice>)this.entities.Products.
+                Where(p => p.UnitPrice != null).
+                OrderByDescending(p => p.UnitPrice.Value).
+                Take(count)
+                .Select(p =>
+                    new ProductPrice
+                    {
+                        Name = p.ProductName,
+                        Price = p.UnitPrice ?? 0,
+                    });
+
+            var result = await Task<IEnumerable<ProductPrice>>.Factory.FromAsync(query.BeginExecute(null, null), (ar) =>
+            {
+                return query.EndExecute(ar);
+            });
+
+            List<ProductPrice> productPrices = new List<ProductPrice>();
+            foreach (var product in result)
+            {
+                productPrices.Add(product);
+            }
+
+            return new ProductReport<ProductPrice>(productPrices);
         }
     }
 }
