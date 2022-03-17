b8c14ecc2d9e36748351546ef5e58ed46a490703
Yury Golub <yuragolub21@gmail.com>
2022-03-17 12:28:33 +0300

    Change sql scripts.

---
+++ Northwind.ReportingServices.SqlService/Sql scripts/dbo.CreateProcedures.sql
@@ -0,0 +1,39 @@
+﻿GO
+CREATE PROCEDURE [dbo].[GetAllProducts ]
+AS
+	SELECT * FROM Products 
+
+GO
+CREATE PROCEDURE [dbo].[GetMostExpensiveProducts]
+	@count int
+AS
+	SET ROWCOUNT @count
+	SELECT * FROM Products
+	ORDER BY UnitPrice DESC
+
+GO
+CREATE PROCEDURE [dbo].GetPriceBetweenProducts
+	@value1 money,
+	@value2 money
+AS
+	SELECT * FROM Products
+	WHERE UnitPrice BETWEEN @value1 AND @value2
+
+GO
+CREATE PROCEDURE [dbo].GetPriceAboveAverageProducts
+AS
+	SELECT * FROM Products
+	WHERE UnitPrice > ALL (SELECT AVG(UnitPrice) FROM Products);
+
+GO
+CREATE PROCEDURE [dbo].GetPriceLessThanProducts
+	@value money
+AS
+	SELECT * FROM Products
+	WHERE UnitPrice < @value
+
+GO
+CREATE PROCEDURE [dbo].GetUnitsInStockDeficitProducts
+AS
+	SELECT * FROM Products
+	WHERE UnitsInStock < UnitsOnOrder
